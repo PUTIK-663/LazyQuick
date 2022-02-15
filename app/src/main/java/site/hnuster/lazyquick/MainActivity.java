939fe27717b8b53159b123a2c1c665a740d3ea2f
PUTIK-663 <2655704309@qq.com>
2022-02-15 22:44:18 +0800

    cleaned some of the code,and normalized annotation

--- app/src/main/java/site/hnuster/lazyquick/MainActivity.java
+++ app/src/main/java/site/hnuster/lazyquick/MainActivity.java
@@ -2,14 +2,13 @@ package site.hnuster.lazyquick;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Context;
-import android.os.Build;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
@@ -17,18 +16,61 @@ public class MainActivity extends AppCompatActivity {
 
     Context thisContext = this;
     private long exitTime = 0;
+    private ImageView userPortraitImg;
+    private Button userStatusBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        * 为了设置沉浸式状态栏，由于其余方法太麻烦，这里使用以下方法：
+        * 1.主题配置页面将状态栏设置透明
+        * 2.Activity里设置topContent内间距将状态栏部分填满
+        * 3.设置topContent为纯色，模拟沉浸式状态栏
+        */
         LinearLayout topContent = findViewById(R.id.top_content);
-//        LinearLayout.MarginLayoutParams params = (LinearLayout.MarginLayoutParams) topContent.getLayoutParams();
-//        params.setMargins(0,getStatusBarHeight(),0,0);
-//        topContent.setLayoutParams(params);
         topContent.setPadding(5,getStatusBarHeight(),5,5);
+        //检查登录状态
     }
 
+    /**
+     *@author putik-663
+     *@Time 2022/2/15
+     *@Description 初始化各组件
+     */
+    public void initComponent(){
+       userPortraitImg = findViewById(R.id.user_portrait);
+       userStatusBtn = findViewById(R.id.user_status);
+    }
+
+    /**
+     *@author putik-663
+     *@Time 2022/2/15
+     *@Description 每次页面onRestart时都再次检查网络连接以及登录状态
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        //1.检查网络连接
+        if (!checkNetworkStatus()){
+            Toast.makeText(thisContext, "网络连接异常", Toast.LENGTH_SHORT).show();
+        }else {
+            //2.网络连接无误则检查登录状态并更新资料
+            if (checkLoginStatus()){
+                //TODO
+                //userPortraitImg.setImageBitmap();
+                //userStatusBtn.setText(username);
+            }
+        }
+        System.out.println("初始化完毕...");
+    }
+
+    /**
+     *@author putik-663
+     *@Time 2022/2/15
+     *@Description 用户误触返回键友好提示，当用户本次点击返回键与上一次点击的时间间隔超过两秒，则提示用户是否确定退出
+     */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if(keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_DOWN){
@@ -45,7 +87,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * 利用反射获取状态栏高度
+     *@author putik-663
+     *@Time 2022/2/15
+     *@Description 利用反射获取状态栏高度
      */
     public int getStatusBarHeight() {
         int result = 0;
@@ -55,5 +99,27 @@ public class MainActivity extends AppCompatActivity {
             result = getResources().getDimensionPixelSize(resourceId);
         }
         return result;
+    }
+
+    /**
+     *@author putik-663
+     *@Time 2022/2/15
+     *@Description 检查网络连接状态
+     */
+    //TODO 目前还不了解原理
+    public boolean checkNetworkStatus(){
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        //直接返回是否连接的布尔值
+        return networkInfo != null && networkInfo.isConnected();
+    }
+
+    /**
+     *@author putik-663
+     *@Time 2022/2/15
+     *@Description 检查本地登录状态记录
+     */
+    public boolean checkLoginStatus(){
+        return true;
     }
 }
