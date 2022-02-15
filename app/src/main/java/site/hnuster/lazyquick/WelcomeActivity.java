ac4cd8da9ef20f3b7fdd7146f50fb3cdc8251736
PUTIK-663 <2655704309@qq.com>
2022-02-15 22:14:58 +0800

    alter the welcome page's function

--- app/src/main/java/site/hnuster/lazyquick/WelcomeActivity.java
+++ app/src/main/java/site/hnuster/lazyquick/WelcomeActivity.java
@@ -1,13 +1,18 @@
 package site.hnuster.lazyquick;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+/**
+ *@author putik-663
+ *@Time 2022/2/15
+ *@Description 欢迎界面，未来可能考虑利用在此界面延迟的三秒为主界面预加载资源，现在还不知道要干啥
+ */
 public class WelcomeActivity extends AppCompatActivity {
 
+    //run用于判断用户是否第一次启动本页面，故设置为静态
     private static boolean run = false;
 
     @Override
@@ -24,15 +29,8 @@ public class WelcomeActivity extends AppCompatActivity {
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                System.out.println("初始化中...");
-                /*
-                    1.检查网络连接
-                    2.检查登录状态
-                 */
-                System.out.println("初始化完毕...");
                 run = true;
                 Intent intent = new Intent(WelcomeActivity.this,MainActivity.class);
-                //数据传递
                 startActivity(intent);
                 finish();
             }
